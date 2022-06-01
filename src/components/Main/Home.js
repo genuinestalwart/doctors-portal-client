be9d6be8895d5337f9be74986d6c7a7bc16c953a
genuinestalwart <genuinestalwart@gmail.com>
2022-06-01 23:14:30 +0600

    treatment section done

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Home/Banner';
 import Services from '../Home/Services';
 import Summary from '../Home/Summary';
+import Treatment from '../Home/Treatment';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Summary></Summary>
             <Services></Services>
+            <Treatment></Treatment>
         </div>
     );
 };
